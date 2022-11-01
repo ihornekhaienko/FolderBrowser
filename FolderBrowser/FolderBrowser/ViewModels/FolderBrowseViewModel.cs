322e02f2d00f0e4aea03e9f9abb1a727dac80ca0
Igor <70515099+ihornekhaienko@users.noreply.github.com>
2022-11-01 14:54:41 +0200

    Add files via upload

---
+++ FolderBrowser/FolderBrowser/ViewModels/FolderBrowseViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace FolderBrowser.ViewModels
+{
+    public class FolderBrowseViewModel
+    {
+        public string? Name { get; set; } //folder name
+        public string? Path { get; set; } //redirection path
+    }
+}
